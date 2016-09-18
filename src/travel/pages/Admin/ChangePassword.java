9fd9b4fc273caf045ea17d94635bf53010af6e4b
Akshaypokley <akshay85pokley@gmail.com>
2016-09-18 17:52:59 +0530

    Today I Developed Change Password Page

---
+++ src/travel/pages/Admin/ChangePassword.java
@@ -0,0 +1,81 @@
+package travel.pages.Admin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.security.PublicKey;
+
+/**
+ * Created by Admin on 18/09/2016.
+ */
+public class ChangePassword {
+
+    WebDriver driver;
+    @FindBy(xpath = "//h1")
+    public WebElement PageTitle;
+
+    @FindBy(linkText = "Old Password")
+    public WebElement OpLable1;
+
+    @FindBy(id="oldPassword")
+    public WebElement OldPassword;
+
+    @FindBy(linkText = "New Password")
+    public WebElement NewPlable;
+
+    @FindBy(id="Password")
+    public WebElement NewPass;
+
+    @FindBy(linkText = "Confirm Password")
+    public WebElement ReTyPlable;
+
+    @FindBy(id="ConfirmPassword")
+    public WebElement RetPassword;
+
+    @FindBy(id="btnSubmit")
+    public  WebElement SaveBtn;
+
+    @FindBy(linkText = "Cancel")
+    public WebElement canBtn;
+
+    ChangePassword(WebDriver driver){
+
+        PageFactory.initElements(driver,true);
+        if(!PageTitle.isDisplayed())
+            throw new IllegalStateException("this is not Change Password page");
+    }
+
+    public WebElement   getPageTitle()
+    {
+    return  PageTitle;
+    }
+
+    public void setOldPassword(String OldPassword1)
+    {
+        OldPassword.sendKeys(OldPassword1);
+    }
+
+    public  void setNewPass(String NewPassword)
+    {
+        NewPass.sendKeys(NewPassword);
+    }
+
+    public void setRetPassword(String ReTypePassword1)
+    {
+        RetPassword.sendKeys(ReTypePassword1);
+    }
+
+    public void clickChangeBtn()
+    {
+    SaveBtn.click();
+    }
+
+    public void ClickCancelBtn()
+    {
+        canBtn.click();
+    }
+
+
+}
