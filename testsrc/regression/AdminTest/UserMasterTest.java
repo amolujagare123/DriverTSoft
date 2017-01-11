c5fa012421de11c93230bac3f1443817df5bf997
Akshay Pokley <akshay85pokley@gmail.com>
2017-01-11 11:57:21 +0530

    f

--- testsrc/regression/AdminTest/UserMasterTest.java
+++ testsrc/regression/AdminTest/UserMasterTest.java
@@ -1,7 +1,6 @@
 package regression.AdminTest;
 
-import jxl.Sheet;
-import jxl.Workbook;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -19,7 +18,6 @@ import org.testng.annotations.Test;
 
 
 
-import jxl.read.biff.BiffException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -57,14 +55,15 @@ public class UserMasterTest {
 
         login.setUsername("akshay05pokley@gmail.com");
 
-        login.setPassword("fg3052FG");
+        login.setPassword("bg1352BG");
 
         login.clickButtonSubmit();
     }
 
     @Test(dataProvider = "userInput")
 
-    public void UserTest(String Employeename) {
+    public void UserTest(String Employeename,String Email,String password,String repassword,String designation,
+                         String mobile,String address,String Expected,String Actual) {
 
         try {
 
@@ -75,24 +74,38 @@ public class UserMasterTest {
             UserMaster usermaster = new UserMaster(driver);
             usermaster.ClickAddnew();
             usermaster.setEmployeeName(Employeename);
-        } catch (AssertionError e) {
-            System.out.println("a");
+            usermaster.setEmail(Email);
+            usermaster.setPassword(password);
+            usermaster.setReTypePassword(repassword);
+            usermaster.setDesignation(designation);
+            usermaster.setMobile(mobile);
+            usermaster.setAddress(address);
+            usermaster.ClickBtnSave();
+            usermaster.setClickYes();
+            String actual = "";
+            try {
+                actual = driver.findElement(By.linkText(Actual)).getText();
+                Assert.assertEquals(actual.equals(Expected), "Test Pass");
+            } catch (AssertionError e) {
+                System.out.println("a");
+            }
+        } catch (Exception e) {
+
         }
     }
-
-    @DataProvider
+        @DataProvider
 
     public Object[][] userInput() throws IOException {
         // String[][] data = new String[0][1];
 
-        FileInputStream fileInputStream = new FileInputStream("Excelsheet/new.xls");
+        FileInputStream fileInputStream = new FileInputStream("Excelsheet/admintest.xls");
 
         HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
 
-        HSSFSheet worksheet = workbook.getSheet("new");
+        HSSFSheet worksheet = workbook.getSheet("u");
         int rowcount=worksheet.getPhysicalNumberOfRows();
        // int rowcount = worksheet.getLastRowNum();
-        String[][] data = new String[rowcount - 1][1];
+        String[][] data = new String[rowcount - 1][9];
         for (int i = 1; i < rowcount; i++) {
             HSSFRow row = worksheet.getRow(i);
 
@@ -103,6 +116,66 @@ public class UserMasterTest {
                 Employee_nameCell.setCellType(Cell.CELL_TYPE_STRING);
                 data[i - 1][0] = Employee_nameCell.getStringCellValue();
             }
+
+            HSSFCell EmailCell = row.getCell(1);
+            if (EmailCell  == null) {
+                data[i - 1][1] = "";
+            } else {
+                EmailCell .setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][1] = EmailCell .getStringCellValue();
+            }
+
+            HSSFCell PasswordCell = row.getCell(2);
+            if (PasswordCell == null) {
+                data[i - 1][2] = "";
+            } else {
+                PasswordCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][2] = PasswordCell.getStringCellValue();
+            }
+
+            HSSFCell re_PasswordCell = row.getCell(3);
+            if (re_PasswordCell == null) {
+                data[i - 1][3] = "";
+            } else {
+                re_PasswordCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][3] = re_PasswordCell.getStringCellValue();
+            }
+
+            HSSFCell DesignCell = row.getCell(4);
+            if (DesignCell == null) {
+                data[i - 1][4] = "";
+            } else {
+                DesignCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][4] = DesignCell.getStringCellValue();
+            }
+
+
+            HSSFCell MobileCell = row.getCell(5);
+            if (MobileCell  == null) {
+                data[i - 1][5] = "";
+            } else {
+                MobileCell .setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][5] = MobileCell .getStringCellValue();
+            }
+
+
+            HSSFCell AddressCell = row.getCell(6);
+            if (AddressCell  == null) {
+                data[i - 1][6] = "";
+            } else {
+                AddressCell .setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][6] = AddressCell.getStringCellValue();
+            }
+
+            HSSFCell ExpetedCell = row.getCell(7);
+            if (ExpetedCell  == null) {
+                data[i - 1][7] = "";
+            } else {
+                ExpetedCell .setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][7] = ExpetedCell.getStringCellValue();
+            }
+
+
 
         }
         return data;
